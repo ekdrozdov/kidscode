882b84c55e89f66027a2e613d4efdffc3e9ae631
Eduard <ekdrozdov@gmail.com>
2018-05-07 07:12:50 +0700

    description update

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # kidscode
 This is a translator for C++.
 
+var 4
+rgz v ?
+
 Features:
+data types: int
+variable description instructions
+assignment operator, cycle "for" (any nesting)
++, -, *, &&, '||' operators
--- main.cpp
+++ main.cpp
@@ -24,38 +24,5 @@ int main() {
 	string filename = "sample.txt";
 	la.scanCode(filename);
 
-	/* LAB 1 demonstration code
-	string _keywords[3] = {"main", "int", "return"};
-	string _delimiters[5] = {";", "(", ")", "{", "}"};
-	string _signs[2] = {"+", "="};
-	string _additional[0] = {};
-
-	ConstTable keywords = ConstTable(_keywords, 3);
-	ConstTable delimiters = ConstTable(_delimiters,  5);
-	ConstTable signs = ConstTable(_signs, 2);
-	ConstTable additional = ConstTable(_additional, 0);
-
-	HashTable variables = HashTable(100);
-	cout << "Table size set as " << variables.getSize() << endl;
-	cout << "--- VALUES SETTING ---\n";
-	cout << "Let's add three variables: 'x', 'yVar' and 'z'" << endl;
-	variables.add("x");
-	variables.add("yVar");
-	variables.add("z");
-	variables.print();
-	cout << "--- SEARCHING ---\n";
-	cout << "Let's search for 'x' and 'y' by names" << endl;
-	cout << variables.find("x") << endl;
-	cout << variables.find("y") << endl;
-	cout << "--- MODYFYING ---\n";
-	cout << "Let's say 'x' and 'z' is define and 'x' type is int" << endl;
-	string name = "x";
-	variables.setDefine(name);
-	variables.setType(name, 1);
-	name = "z";
-	variables.setDefine(name);
-	variables.print();
-	*/
-
 	return 0;
 }
